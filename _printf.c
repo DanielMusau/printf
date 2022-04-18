6b5b7dc4a9a151d32e738ee1085c23978a8a152b
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-18 06:09:51 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - returns the number of characters printed.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
+ */
+int _printf(const char *format, ...)
+{
+
+}
