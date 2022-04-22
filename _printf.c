179968feb4b55cbe7b331db9df632a6f00f8562d
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-22 04:50:27 -0700

    Returned the original files

--- _printf.c
+++ _printf.c
@@ -6,58 +6,6 @@
  *
  * Return: number of chars printed.
  */
-<<<<<<< HEAD
-void v_printf(const char *format, va_list args)
-{
-	char ch;
-	const char *s;
-	int state = 0;
-
-	while (*format)
-	{
-		if (state == 0)
-		{
-			if (*format == '%')
-			{
-				state = 1;
-			}
-			else 
-			{
-				putchar(*format);
-			}
-		} else if (state == 1)
-		{
-			switch (*format)
-			{
-				case 'c':
-					ch = va_arg(args, int);
-					putchar(ch);
-					break;
-				case 's':
-					s = va_arg(args, const char *);
-					while (*s)
-					{
-						putchar(*s++);
-					}
-					break;
-				case 'd':
-
-					break;
-				case 'x':
-
-					break;
-				case 'p':
-					putchar('0');
-					putchar('x');
-					break;
-			}
-			state = 0;
-		}
-		format++;
-	}
-}
-=======
->>>>>>> printf_function
 int _printf(const char *format, ...)
 {
 	int printed_chars;
